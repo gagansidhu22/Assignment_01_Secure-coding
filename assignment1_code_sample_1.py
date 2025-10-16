85b7552295f07e497194e7ca29f44fbf69a25243
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-10-15 19:47:41 -0500

    changed the code according to comments.

--- assignment1_code_sample_1.py
+++ assignment1_code_sample_1.py
@@ -1,33 +1,58 @@
 import os
 import pymysql
 from urllib.request import urlopen
+import smtplib
+from email.message import EmailMessage
+import requests
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS')
 }
+
 
 def get_user_input():
     user_input = input('Enter your name: ')
     return user_input
 
 def send_email(to, subject, body):
-    os.system(f'echo {body} | mail -s "{subject}" {to}')
+    msg = EmailMessage()
+    msg['To'] = to
+    msg['Subject'] = subject
+    msg.set_content(body)
+
+    with smtplib.SMTP('smtp.example.com', 587) as smtp:
+        smtp.send_message(msg)
 
 def get_data():
-    url = 'http://insecure-api.com/get-data'
-    data = urlopen(url).read().decode()
-    return data
+    url = 'https://secure-api.com/get-data'
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    return response.text
 
 def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    query = "INSERT INTO mytable (column1, column2) VALUES (%s, %s)"
+    cursor.execute(query, (data, "Another Value"))
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
     cursor.execute(query)
     connection.commit()
     cursor.close()
     connection.close()
+
+def get_data():
+    try:
+        url = 'https://secure-api.com/get-data'
+        data = urlopen(url).read().decode()
+        return data
+    except Exception as e:
+        logging.error("Error fetching data: %s", e)
+        return None
+
 
 if __name__ == '__main__':
     user_input = get_user_input()
